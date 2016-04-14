97f1f2d614fc576194d88cec951584bdfc070a07
David M. Rivero G <davidmriverog@gmail.com>
2016-04-14 16:27:44 -0430

    ejercicio nro. 9 manejo de grunt con angular

--- eje_009/Gruntfile.js
+++ eje_009/Gruntfile.js
@@ -7,14 +7,26 @@ module.exports = function(grunt) {
         src: 'src/js/*.js',
         dest: 'dist/js/operations.min.js'
       }
+    },
+    cssmin: {
+  options: {
+    shorthandCompacting: false,
+    roundingPrecision: -1
+  },
+  target: {
+    files: {
+      'dist/css/styles.min.css': ['src/css/*.css']
     }
+  }
+}
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','cssmin']);
 
 };
