c389992b73e8d2203a3072fdf12b10235ca9f5a7
David M. Rivero G <davidmriverog@gmail.com>
2016-04-14 22:38:06 -0430

    inicio del ejercicio 12, descarga de yeoman

---
+++ eje_012/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eje012App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the eje012App
+ */
+angular.module('eje012App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ eje_012/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eje012App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eje012App
+ */
+angular.module('eje012App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
